67fd44562d37766f7ae89a198853289c6676e867
Sanskruti-Shete <shetesanskruti066@gmail.com>
2025-06-11 11:18:49 +0530

    api endpoints

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const Transaction = require('./models/Transaction');
+const mongoose = require("mongoose");
+const app =express();
+
+app.use(cors());
+app.use(express.json());
+app.get('/api/test',(req,res)=>{
+    res.json('test ok2');
+});
+
+app.post('/api/transaction',async (req,res)=>{
+    await mongoose.connect(process.env.MONGO_URL);
+    const {name,description,price} = req.body;
+    const transaction=await Transaction.create({name,description,price});
+    res.json(transaction);
+});
+app.listen(4000);
+
--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  function addNewTransaction(ev){
+    ev.preventDefault();
+    const url=process.env.REACT_APP_API_URL+'/transaction';
+    const price = name.split('')[0];
+    fetch(url,{
+      method:'POST',
+      headers:{'Content-type':'application/json'},
+      body: JSON.stringify({price,name:name.substring(price.length+1),description})
+    }).then(response=>{
+      response.json().then(result=>{
+        setName('');
+        setDescription('');
+        console.log('result',result);
+      });
+    });
+  }
+
   return (
     <div className="app-container">
       <div className="balance-section">
         <h1 className="balance-amount">$400.00</h1>
       </div>
-      
-      <div className="input-section">
-        <input 
-          type="text" 
-          placeholder="+200 new samsung tv" 
-          className="amount-input"
-        />
-        <input 
-          type="text" 
-          placeholder="description" 
-          className="description-input"
-        />
-        <button className="add-btn">Add new transaction</button>
-      </div>
-      
+
+      <form onSubmit={addNewTransaction}>
+        <div className="input-section">
+          <input
+            type="text"
+            value={name}
+            onChange={(ev) => setName(ev.target.value)}
+            placeholder="+200 new samsung tv"
+            className="amount-input"
+          />
+          <input
+            type="text"
+            value={description}
+            onChange={(ev) => setDescription(ev.target.value)}
+            placeholder="description"
+            className="description-input"
+          />
+          <button className="add-btn">Add new transaction</button>
+        </div>
+      </form>
+
       <div className="transactions-list">
         <div className="transaction-item">
           <div className="transaction-details">
@@ -33,7 +59,7 @@ function App() {
             <span className="transaction-date">2022-12-18 15:45</span>
           </div>
         </div>
-        
+
         <div className="transaction-item">
           <div className="transaction-details">
             <h3>Gig job new website</h3>
@@ -44,7 +70,7 @@ function App() {
             <span className="transaction-date">2022-12-18 15:45</span>
           </div>
         </div>
-        
+
         <div className="transaction-item">
           <div className="transaction-details">
             <h3>Iphone</h3>
